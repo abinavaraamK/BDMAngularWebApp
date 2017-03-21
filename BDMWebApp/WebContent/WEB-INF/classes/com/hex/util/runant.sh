9a64c6d9b7948183f53e025e3faa37383b58142c
abinavaraamK <krishabinav93@gmail.com>
2017-03-21 17:44:19 +0530

    changes for building sh file

---
+++ BDMWebApp/WebContent/WEB-INF/classes/com/hex/util/runant.sh
@@ -0,0 +1,5 @@
+echo triggering ant script from java
+
+echo "triggering ant script from java"
+
+ant -buildfile %2 %1%
--- BDMWebApp/src/com/hex/util/HexBuildGenerator.java
+++ BDMWebApp/src/com/hex/util/HexBuildGenerator.java
@@ -27,20 +27,28 @@ public class HexBuildGenerator {
   }
   
       
-   public void generateBuildProperties(String lsPresentation,String lsBusiness,String lsPersistence,String outDirectory,String warFile,String templatesLocation,String baseLocation) {
-System.out.println("HexBuildGenerator ***********without deploy**************");
- String content = getBuildProperties(outDirectory ,lsPresentation,lsBusiness,lsPersistence,warFile,templatesLocation);
- //String fileName = "HexFrameBuild.properties";
-   String fileName = HexBuildGenerator.class.getClassLoader().getResource("HexFrameBuild.properties").getFile();
- System.out.println(">>>>>>>"+HexBuildGenerator.class.getClassLoader().getResource("HexFrameBuild.properties").getFile());
+    public void generateBuildProperties(String lsPresentation,
+      String lsBusiness, String lsPersistence, String outDirectory,
+      String warFile, String templatesLocation, String baseLocation) {
+    System.out
+        .println("HexBuildGenerator ***********without deploy**************");
+    String content = getBuildProperties(outDirectory, lsPresentation,
+        lsBusiness, lsPersistence, warFile, templatesLocation);
+    // String fileName = "HexFrameBuild.properties";
+    String fileName = HexBuildGenerator.class.getClassLoader()
+        .getResource("HexFrameBuild.properties").getFile();
+    System.out.println(">>>>>>>"
+        + HexBuildGenerator.class.getClassLoader()
+            .getResource("HexFrameBuild.properties").getFile());
 
-   HexUtil.writeFile(content, fileName);
+    HexUtil.writeFile(content, fileName);
     runAnt(baseLocation);
 
   }
 
-   
-  private String getBuildProperties(String outputPath, String psPresentation,String lsBusiness,String lsPersistence,String warFile,String templatesLocation) {
+  private String getBuildProperties(String outputPath, String psPresentation,
+      String lsBusiness, String lsPersistence, String warFile,
+      String templatesLocation) {
 
     System.out.println("Inside generateBuildProperties " + outputPath);
     System.out.println("Inside generateBuildProperties " + psPresentation);
@@ -50,17 +58,23 @@ System.out.println("HexBuildGenerator ***********without deploy**************");
 
     StringBuffer result = new StringBuffer();
     try {
-    	DataInputStream dis = new DataInputStream(new FileInputStream(templatesLocation+
-                "\\templates\\build\\HexFrameBuild.template"));
-    	File file = new File(templatesLocation+"\\Buildlib\\xwork-2.1.2.jar");
-    	  /* InputStream inputStream= UIController.class.getResourceAsStream("\\templates\\templates\\build\\HexFrameBuild.template");
-  		 
-   		DataInputStream dis = new DataInputStream(inputStream);   */
-   	
-   		//File file = new File(UIController.class.getClassLoader().getResource("\\templates\\Buildlib\\xwork-2.1.2.jar").getFile());
-   		String BuildLib= file.getParent();
-   		System.out.println("$$$$$$$$$$$$$$$$$$$"+file.getParent());
-   	
+      DataInputStream dis = new DataInputStream(new FileInputStream(
+          templatesLocation
+              + "/templates/build/HexFrameBuild.template"));
+      File file = new File(templatesLocation
+          + "/Buildlib/xwork-2.1.2.jar");
+      /*
+       * InputStream inputStream= UIController.class.getResourceAsStream(
+       * "\\templates\\templates\\build\\HexFrameBuild.template");
+       * 
+       * DataInputStream dis = new DataInputStream(inputStream);
+       */
+
+      // File file = new
+      // File(UIController.class.getClassLoader().getResource("\\templates\\Buildlib\\xwork-2.1.2.jar").getFile());
+      String BuildLib = file.getParent();
+      System.out.println("$$$$$$$$$$$$$$$$$$$" + file.getParent());
+
       while (dis.available() > 0) {
         String line = dis.readLine();
         System.out.println("Line read " + line);
@@ -76,59 +90,51 @@ System.out.println("HexBuildGenerator ***********without deploy**************");
           line = line.replaceAll("<TableName>", warFile);
         }
 
-       if("IceFaces".equalsIgnoreCase(psPresentation)){
-	        if (line.indexOf("<libdirectory>") >= 0) {
-	              line = line.replaceAll("<libdirectory>", "icefaceslib");
-	        }
-        }
-        else{
-              if (line.indexOf("<libdirectory>") >= 0) {
-            	  BuildLib = BuildLib.replace('\\', '/');
-                  
-            	  line = line.replaceAll("<libdirectory>", BuildLib);
-              }
+        if ("IceFaces".equalsIgnoreCase(psPresentation)) {
+          if (line.indexOf("<libdirectory>") >= 0) {
+            line = line.replaceAll("<libdirectory>", "icefaceslib");
+          }
+        } else {
+          if (line.indexOf("<libdirectory>") >= 0) {
+            BuildLib = BuildLib.replace('\\', '/');
+
+            line = line.replaceAll("<libdirectory>", BuildLib);
+          }
         }
         /*
-        if("IceFaces".equalsIgnoreCase(psPresentation)){
-	        if (line.indexOf("<packagelibdirectory>") >= 0) {
-	              line = line.replaceAll("<packagelibdirectory>", "icefaceslib");
-	        }
-        }
-        else if("Richfaces".equalsIgnoreCase(psPresentation)){
-            if (line.indexOf("<packagelibdirectory>") >= 0) {
-                  line = line.replaceAll("<packagelibdirectory>", "richfaceslib");
-            }
-        }
-        else if("Primefaces".equalsIgnoreCase(psPresentation)){
-            if (line.indexOf("<packagelibdirectory>") >= 0) {
-                  line = line.replaceAll("<packagelibdirectory>", "Primefaceslib");
-            }
-        }
-        else{
-              if (line.indexOf("<packagelibdirectory>") >= 0) {
-            	  line = line.replaceAll("<packagelibdirectory>", "lib");
-              }
-        }        
-*/
-        
+         * if("IceFaces".equalsIgnoreCase(psPresentation)){ if
+         * (line.indexOf("<packagelibdirectory>") >= 0) { line =
+         * line.replaceAll("<packagelibdirectory>", "icefaceslib"); } }
+         * else if("Richfaces".equalsIgnoreCase(psPresentation)){ if
+         * (line.indexOf("<packagelibdirectory>") >= 0) { line =
+         * line.replaceAll("<packagelibdirectory>", "richfaceslib"); } }
+         * else if("Primefaces".equalsIgnoreCase(psPresentation)){ if
+         * (line.indexOf("<packagelibdirectory>") >= 0) { line =
+         * line.replaceAll("<packagelibdirectory>", "Primefaceslib"); }
+         * } else{ if (line.indexOf("<packagelibdirectory>") >= 0) {
+         * line = line.replaceAll("<packagelibdirectory>", "lib"); } }
+         */
+
         if (line.indexOf("<packagelibdirectory>") >= 0) {
-            line = line.replaceAll("<packagelibdirectory>","lib");
-      }
+          line = line.replaceAll("<packagelibdirectory>", "lib");
+        }
         if (line.indexOf("<packagelibdirectoryPresentation>") >= 0) {
-            line = line.replaceAll("<packagelibdirectoryPresentation>",psPresentation+"lib");
-      }
+          line = line.replaceAll("<packagelibdirectoryPresentation>",
+              psPresentation + "lib");
+        }
         if (line.indexOf("<packagelibdirectoryBussiness>") >= 0) {
-            line = line.replaceAll("<packagelibdirectoryBussiness>",lsBusiness+"lib");
-      }
+          line = line.replaceAll("<packagelibdirectoryBussiness>",
+              lsBusiness + "lib");
+        }
         if (line.indexOf("<packagelibdirectoryPersistence>") >= 0) {
-            line = line.replaceAll("<packagelibdirectoryPersistence>",lsPersistence+"lib");
-      }
+          line = line.replaceAll("<packagelibdirectoryPersistence>",
+              lsPersistence + "lib");
+        }
         result.append(line);
         result.append("\n");
       }
       dis.close();
-    }
-    catch (Exception exception) {
+    } catch (Exception exception) {
 
       exception.printStackTrace();
     }
