39813e4624a96c17df30aca9d565b28f8c9ada07
abinavaraamK <krishabinav93@gmail.com>
2017-06-14 13:13:06 +0530

    tty problem

--- BDMWebApp/WebContent/templates/templates/connectBluemix.sh
+++ BDMWebApp/WebContent/templates/templates/connectBluemix.sh
@@ -1,8 +1,8 @@
 echo connectBluemix java
 
-ssh -t remotehost sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
+ssh -t remotehost "sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo"
 
-ssh -t remotehost sudo yum install cf-cli
+ssh -t remotehost "sudo yum install cf-cli"
 
 dpkg -l | grep cf
 
