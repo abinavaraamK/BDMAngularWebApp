56b76901686b0f1a7a725492deef831214fb6438
abinavaraamK <krishabinav93@gmail.com>
2017-06-14 12:19:23 +0530

    install cf yum

--- BDMWebApp/WebContent/templates/templates/connectBluemix.sh
+++ BDMWebApp/WebContent/templates/templates/connectBluemix.sh
@@ -1,11 +1,15 @@
 echo connectBluemix java
 
+sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
+
+sudo yum install cf-cli
+
 dpkg -l | grep cf
 
-echo "connectBluemix java1"
+#echo "connectBluemix java1"
 
-whereis -b cf
+#whereis -b cf
 
-echo "connectBluemix java2"
+#echo "connectBluemix java2"
 
-dpkg --status cf
+#dpkg --status cf
